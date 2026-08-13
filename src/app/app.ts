2604d78072499909079b5db861c8f7ecf5eaaf48
Pamela Carvalho <pamelakaroline004@gmail.com>
2026-08-12 21:14:38 -0300

    criação do componente cadastro atleta

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { AtletaComponent } from './component/atleta-component/atleta-component';
 import { MenuComponent } from './component/menu-component/menu-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuComponent],
+  imports: [RouterOutlet, MenuComponent, AtletaComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
