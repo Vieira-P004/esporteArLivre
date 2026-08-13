5e143a13e69e2b7488c976186aa207cd14b43e4f
Pamela Carvalho <pamelakaroline004@gmail.com>
2026-08-12 21:41:46 -0300

    rotas das paginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+import { AtletaComponent } from './component/atleta-component/atleta-component';
+import { HomeComponent } from './component/home-component/home-component';
+
+export const routes: Routes = [
+    {
+        //joga para página inicial se a url ficar vazia
+        path:'',
+        redirectTo:"/home",
+        pathMatch: 'full'
+    }
+    {
+        path:"/home",
+        component: HomeComponent
+    },
+    {
+        path:"cadastroAtleta",
+        component:AtletaComponent
+    }
+];
