8a7efa7f31f6096bd0f0483f0b2151353b182ce8
Pamela Carvalho <pamelakaroline004@gmail.com>
2026-08-13 20:30:12 -0300

    atualizações

--- src/app/component/atleta-component/atleta-component.ts
+++ src/app/component/atleta-component/atleta-component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-atleta-component',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './atleta-component.html',
   styleUrl: './atleta-component.css',
 })
-export class AtletaComponent {}
+export class AtletaComponent {
+
+  nome = ''
+  cpf = ''
+  sexo = ''
+  cep = ''
+  ruaLogradouro = ''
+  bairro = ''
+  cidade = ''
+  uf = ''
+
+  //DECLARANDO AS FUNÇÕES
+  exibirDados(){
+    console.log(this.nome, this.cpf, this.cep, this.sexo, this.ruaLogradouro, this.bairro, this.cidade, this.uf)
+  }
+}
