8f0b5cee59d03b8be49737066614800f3df8fa78
Pamela Carvalho <pamelakaroline004@gmail.com>
2026-08-14 21:32:27 -0300

    criando componente cadastro corrida

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 
 import { AtletaComponent } from './component/atleta-component/atleta-component';
 import { HomeComponent } from './component/home-component/home-component';
+import { CorridaComponent } from './component/corrida-component/corrida-component';
 
 export const routes: Routes = [
     {
@@ -18,5 +19,9 @@ export const routes: Routes = [
     {
         path:"cadastroAtleta",
         component:AtletaComponent
+    },
+    {
+        path:"cadastroCorrida",
+        component:CorridaComponent
     }
 ];
